2307eb923e64d084c2dfe9af2239b7b2aaffcd4d
constantinoi <pescaalma@gmail.com>
2022-10-18 21:39:20 -0300

    pipes

--- src/app/diretivas-atributos/diretivas-atributos.component.ts
+++ src/app/diretivas-atributos/diretivas-atributos.component.ts
@@ -13,6 +13,8 @@ export class DiretivasAtributosComponent implements OnInit {
   public nome: string = "";
   public list: Array<{ nome: string }> = [{nome:"consta"}];
 
+  public date:Date = new Date();
+
   constructor() { }
 
   ngOnInit(): void {
